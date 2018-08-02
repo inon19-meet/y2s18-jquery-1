d47a7bf6a2a5709f389feda096cdbea0e91b6c1d
= <=>
2018-08-02 18:32:55 +0300

    l

--- events/events.js
+++ events/events.js
@@ -1,5 +1,11 @@
 // Write all your JavaScript and JQuery code in this file! :)
 /* START PROVIDED CODE */
+$("#moveButton").on("click",function()
+{
+	$("#message").css("top", randomY());
+	$("#message").css("left", randomX());
+});
+
 function randomX() {
   return Math.random() * $(window).width();
 }
